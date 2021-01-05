eed0b396aed57710324a0055781893f745c680af
hheimerd <hheimerd@yandex.ru>
2021-01-05 17:37:20 +0300

    DB: add notification table and model

---
+++ database/migrations/2021_01_05_142028_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipient_id')->constrained('users');
+            $table->foreignId('sender_id')->constrained('users');
+            $table->string('activity_type');
+            $table->string('url');
+            $table->boolean('is_unread')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
